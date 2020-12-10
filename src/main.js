0390f721c9db8add801db5b03ff1c4fe90cbccda
JuanDiegoTerriquezPolanco <jterriquez0@ucol.mx>
2020-12-10 14:22:06 -0600

    added mising test files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
-export default class App {}
-
+export default class App {
+    sumatoriaSerieUno(numero){
+        for(let i=0;i<=numero;i++){
+            i = i + 1;
+        }
+        return i;
+    }
+}
 let app = new App();
+console.log(app.sumatoriaSerieUno(7));
