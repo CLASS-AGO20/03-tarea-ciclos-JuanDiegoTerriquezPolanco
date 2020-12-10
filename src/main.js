6e3f0ca2839d747277c42040b09850530995279d
JuanDiegoTerriquezPolanco <jterriquez0@ucol.mx>
2020-12-10 17:36:39 -0600

    implemented obtenerImpares

--- src/main.js
+++ src/main.js
@@ -39,9 +39,30 @@ export default class App {
         } while (i<=fin);
         return aux;
     }
+    obtenerImpares(numero1, numero2){
+        let aux="",inicio=numero1,fin=numero2,aux1;
+        if(numero1>numero2){
+            inicio=numero2;
+            fin=numero1;
+        }
+        aux1=1;
+        for (let i = fin; i >= inicio; i--) {
+            if(i%2!=0){
+                if(aux1==1){
+                    aux=aux+i;
+                    aux1++;
+                }else{
+                    aux=aux+","+i;
+                }
+            }
+        }
+        return aux;
+    }
 }
 let app = new App();
 app.sumatoriaSerieUno(15);
 app.sumatoriaSerieDos(7);
 console.log(app.esPrimo(15));
 console.log(app.obtenerMultiplos(20,35));
+console.log(app.obtenerImpares(5,10));
+console.log(app.obtenerImpares(10,5));
